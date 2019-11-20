ec65819dee7fbc47ce06fc2d3978ace3eaac3a38
TBone-R <trivolet@gmail.com>
2019-11-20 12:03:09 +0100

    add 'if main' to labyrinth.py

--- labyrinth.py
+++ labyrinth.py
@@ -9,30 +9,34 @@ import source.view as view
 from source.constants import *
 
 
-list_item = [model.Item(i) for i in ['item1', 'item2', 'item3']]
+def main():
 
-player = model.Player(list_item)
+    list_item = [model.Item(i) for i in ['item1', 'item2', 'item3']]
 
-
-pygame.init()
-window = pygame.display.set_mode((DIMENSION_LABYRINTH, DIMENSION_LABYRINTH))
-running = True
-
-key_arrow = {pygame.K_UP: 'north', pygame.K_DOWN: 'south', pygame.K_RIGHT: 'east', pygame.K_LEFT: 'west'}
-
-while running:
-    for event in pygame.event.get():
-
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key in key_arrow.keys():
-                direction = key_arrow[event.key]
-                player.move_to(direction)
-
-        window.blit(view.Labyrinth(player.labyrinth), (0, 0))
-        window.blit(*view.Player(player).blit())
-
-        pygame.display.update()
+    player = model.Player(list_item)
 
 
+    pygame.init()
+    window = pygame.display.set_mode((DIMENSION_LABYRINTH, DIMENSION_LABYRINTH))
+    running = True
+
+    key_arrow = {pygame.K_UP: 'north', pygame.K_DOWN: 'south', pygame.K_RIGHT: 'east', pygame.K_LEFT: 'west'}
+
+    while running:
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key in key_arrow.keys():
+                    direction = key_arrow[event.key]
+                    player.move_to(direction)
+
+            window.blit(view.Labyrinth(player.labyrinth), (0, 0))
+            window.blit(*view.Player(player).blit())
+
+            pygame.display.update()
+
+
+if __name__ == '__main__':
+    main()
