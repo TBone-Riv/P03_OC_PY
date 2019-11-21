09e25ed8b6eef27e1f44d9953e80cae9e0961fcf
TBone-R <trivolet@gmail.com>
2019-11-21 16:36:00 +0100

    add 1 get an 2 set method

---
+++ model/labyrinth.py
@@ -0,0 +1,85 @@
+from random import randint
+from model.box import Box
+
+
+class Labyrinth:
+    """Decrypt and stock the structure of labyrinth"""
+
+    def __init__(self, file="./resource/labyrinth"):
+
+        self.file = file
+        self.matrix = []
+
+        self.build()
+
+    def get_box(self, line, column):
+        """Take coordinate and return a box"""
+        return self.matrix[line][column]
+
+    def set_box_content(self, line, column, content):
+        """Take coordinate and a content to assign to a box"""
+        self.get_box(line, column).content = content
+
+    def emptying_box(self, line, column):
+        """Take coordinate and set a box content to None"""
+        self.get_box(line, column).content = None
+
+    def build(self):
+        """Build labyrinth from an external file"""
+
+        key = {"□": [],
+               "←": ["west"],
+               "↑": ["north"],
+               "→": ["east"],
+               "↓": ["south"],
+               "━": ["west", "east"],
+               "┃": ["north", "south"],
+               "┏": ["south", "east"],
+               "┓": ["south", "west"],
+               "┗": ["north", "east"],
+               "┛": ["north", "west"],
+               "┣": ["north", "south", "east"],
+               "┫": ["north", "south", "west"],
+               "┳": ["south", "west", "east"],
+               "┻": ["north", "west", "east"],
+               "╋": ["north", "south", "west", "east"]
+               }
+
+        with open(self.file, 'r', errors='ignore', encoding="UTF8") as file:
+            read_lab = file.read()
+
+        for wall_line in read_lab.split('\n'):
+            list_wall = []
+
+            for wall in wall_line:
+                list_wall.append(Box(**{k: True for k in key[wall]}))
+
+            self.matrix.append(list_wall)
+
+    def get_valid_box(self):
+        """Return random box coordinate """
+
+        # Check if an accessible box exist
+        if [[x.is_valid for x in y] for y in self.matrix] ==\
+                [[False for _ in y] for y in self.matrix]:
+            raise Exception('No valid Box')
+
+        # Get a first random box coordinate
+        line = randint(0, len(self.matrix) - 1)
+        column = randint(0, len(self.matrix[line]) - 1)
+
+        # Repeat the accusation while box isn't accessible
+        while not self.matrix[line][column].is_valid:
+            line = randint(0, len(self.matrix) - 1)
+            column = randint(0, len(self.matrix[line]) - 1)
+
+        return line, column
+
+    def valid_move(self, box_position, direction):
+        """Check cardinal movement"""
+        return getattr(self.matrix[box_position[0]][box_position[1]], direction)
+
+    def event(self, player, box_position):
+        """Call "Box.event" method if exist"""
+        if self.matrix[box_position[0]][box_position[1]].content is not None:
+            self.matrix[box_position[0]][box_position[1]].content.event(player)
